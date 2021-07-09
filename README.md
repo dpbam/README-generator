648a0d53378bbf3c478aa663aa0723ad3f9c72f5
Derek Phelps <derekphtpt@gmail.com>
2021-07-09 15:38:25 -0600

    getting stuff to appear now, working on license badge

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# undefined
- ## Description undefined
+# readme generatorrrrrr
+ 
+    ## License:
+    [![License](https://img.shields.io/badge/License-Apache-2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+
+    ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - 
+
+    ## Description generates a readme
+
+
+    ## Contact info:
+    -GitHub: [dpbam](https://github.com/dpbam)
+    -email: derek@hotmail.com
+    
+    
--- index.js
+++ index.js
@@ -7,32 +7,32 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
     questions = [
-        // {
-        //     type: 'input',
-        //     name: 'github',
-        //     message: 'What is your GitHub username?',
-        //     validate: githubName => {
-        //         if (githubName) {
-        //             return true;
-        //         } else {
-        //             console.log("Try again.")
-        //             return false;
-        //         }
-        //     }
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'email',
-        //     message: 'What is your email address?',
-        //     validate: email => {
-        //         if (email) {
-        //             return true;
-        //         } else {
-        //             console.log("Try to write your email again.")
-        //             return false;
-        //         }
-        //     }
-        // },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+            validate: githubName => {
+                if (githubName) {
+                    return true;
+                } else {
+                    console.log("Try again.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+            validate: email => {
+                if (email) {
+                    return true;
+                } else {
+                    console.log("Try to write your email again.")
+                    return false;
+                }
+            }
+        },
         {
             type: 'input',
             name: 'title',
@@ -59,25 +59,39 @@ const fs = require('fs');
                 }
             }
         },
-        // {
-        //     type: 'input',
-        //     name: 'tableOfContents',
-        //     message: 'If your README is very long, add a table of contents to make it easy for users to find what they need. (optional)',
-        //     validate: tableOfContents => {
-        //         if (tableOfContents) {
-        //             return true;
-        //         } else {
-        //             return false;
-        //         }
-        //     }
-        // },
-        // {
-        //     type: 'checkbox',
-        //     name: 'languages',
-        //     message: 'What did you build this project with? (Check all that apply)',
-        //     choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        {
+            type: 'input',
+            name: 'tableOfContents',
+            message: 'If your README is very long, add a table of contents to make it easy for users to find what they need. (optional)',
+            validate: tableOfContents => {
+                if (tableOfContents) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
 
-        // }
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Choose a license from the list that suits your project.',
+            choices: ['Apache-2.0','IBM', 'MIT', 'Eclipse'],
+            validate: userLicense => {
+                if (userLicense) {
+                    return true;
+                } else {
+                    console.log("Select a license for the project.")
+                    return false;
+                }
+            }
+        }
     ]
 
     
@@ -97,14 +111,31 @@ function init() {
     console.log("Welcome to the README.md generator 2000.\nPlease answer some questions to get started.")
     inquirer.prompt(questions)
     .then(questionValues => {
-        var stringQuestionValues = JSON.stringify(questionValues);
+        var stringQuestionValues = questionValues;
 
     writeToFile('README.md', generateMarkdown(stringQuestionValues));
     })
 };
 
 function generateMarkdown(data) {
-    return "README.md", `# ${data.title}\n ## Description ${data.description}\n`
+    return "README.md", `# ${data.title}\n 
+    ## License:
+    [![License](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})
+
+
+    ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - 
+
+    ## Description ${data.description}\n
+
+    ## Contact info:
+    -GitHub: [${data.github}](https://github.com/${data.github})
+    -email: ${data.email}
+    
+    `
+
 
 
 }
